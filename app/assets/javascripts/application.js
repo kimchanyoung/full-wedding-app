9e713f998360f6d428ba155ebbc0c7071849cfe8
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2016-02-26 21:01:24 -0500

    Gemfile and foundationapps

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,38 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require foundation
 //= require_tree .
+
+
+$(function(){ $(document).foundation();
+  $('#about-btn').click(function(e){
+    $('#wedding-content').hide();
+    $('#registry-content').hide();
+    $('#rsvp-content').hide();
+    $('#about-content').show();
+  });
+
+  $('#wedding-btn').click(function(e){
+    $('#about-content').hide();
+    $('#registry-content').hide();
+    $('#rsvp-content').hide();
+    $('#wedding-content').show();
+  });
+
+  $('#registry-btn').click(function(e){
+    $('#about-content').hide();
+    $('#wedding-content').hide();
+    $('#rsvp-content').hide();
+    $('#registry-content').show();
+  });
+
+  $('#rsvp-btn').click(function(e){
+    $('#about-content').hide();
+    $('#wedding-content').hide();
+    $('#registry-content').hide();
+    $('#rsvp-content').show();
+  });
+
+
+});
