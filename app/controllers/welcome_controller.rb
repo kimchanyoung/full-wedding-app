42a42ef98e0e3a635e5f64b5d27522b052b01ef9
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2016-03-15 23:27:03 -0400

    Add password screen

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,17 @@
 class WelcomeController < ApplicationController
-
   def index
+    if params[:password] == 'farts'
+      cookies[:logged_in] = true
+    end
+
+    if cookies[:logged_in] == true
+      render 'index'
+    else
+      render 'under_construction'
+    end
+  end
+
+  def under_construction
+
   end
 end
