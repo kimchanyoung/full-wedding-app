52b5cedc9f5a35512d08ea785565227eb6a8242c
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2016-03-29 02:41:15 -0400

    Remove unnec

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.cache_classes = true
-  config.serve_static_assets = true
   config.assets.compile = true
   config.assets.digest = true
 end
