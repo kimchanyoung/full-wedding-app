111622aad981f59cbc4d98c888f469dfcbf23085
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2016-03-31 20:15:47 -0400

    favicon and link changes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,17 +1,4 @@
 class WelcomeController < ApplicationController
   def index
-    if params[:password] == 'printers'
-      cookies[:logged_in] = true
-    end
-
-    if cookies[:logged_in] == true
-      render 'index'
-    else
-      render 'under_construction'
-    end
-  end
-
-  def under_construction
-
   end
 end
